f1df4aabfd514150af2c0c240666204f1a5d7473
Lucian Mitrofan <lucmit214@gmail.com>
2022-10-25 00:12:38 +0300

    Add more loading/unloadings inputs

--- static/js/main.js
+++ static/js/main.js
@@ -2,30 +2,33 @@
 const form = document.querySelector('form');
 const loadingDivContainer = document.querySelector('.loadings');
 const unloadingDivContainer = document.querySelector('.unloadings');
-const addLoadingBtn = document.querySelector('.add-loading');
-const addUnloadingBtn = document.querySelector('.add-unloading');
-const loadCount = 1;
-const unloadCount = 1;
+const addLoadingBtn = document.querySelector('#addLoad');
+const addUnloadingBtn = document.querySelector('#addUnload');
+let loadCount = 1;
+let unloadCount = 1;
 // create input element for each truck
 
 function addLoading() {
     loadCount++
-    const load = `
-    <div class="load">
+    const load = document.createElement('div');
+    load.classList.add('load');
+    load.innerHTML = `
         <input type="text" name="loadCompany${loadCount}" placeholder="Loading Place" value="loadcom${loadCount}">
         <input type="text" name="loadAddress${loadCount}" placeholder="Loading Place" value="loadaddr${loadCount}">
-    </div>
     `
-    loadingDivContainer.appendChild(load);
+    loadingDivContainer.append(load);
 }
 
 function addUnloading() {
     unloadCount++
-    const unload = `
-    <div class="unload">
+    const unload = document.createElement('div');
+    unload.classList.add('unload');
+    unload.innerHTML = `
         <input type="text" name="unloadCompany${unloadCount}" placeholder="Unloading Place" value="unloadcom${unloadCount}">
         <input type="text" name="unloadAddress${unloadCount}" placeholder="Unloading Place" value="unloadaddr${unloadCount}">
-    </div>
     `
-    unloadingDivContainer.appendChild(unload); 
+    unloadingDivContainer.append(unload);
 }
+
+addLoadingBtn.addEventListener('click', addLoading);
+addUnloadingBtn.addEventListener('click', addUnloading);
